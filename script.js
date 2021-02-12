e2df74ca82e4690228119be0c307cca5a867f2d0
Kadorado <Kadorado>
2021-02-12 23:33:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
+
+jokeBtn.addEventListener("click", generateJoke);
+
+// USING ASYNC/AWAIT
+
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
+
+  try {
+    const res = await fetch("https://icanhazdadjoke.com", config);
+
+    const data = await res.json();
+
+    jokeEl.innerHTML = data.joke;
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+//   HINTS
+// USING .then()
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       Accept: 'application/json',
+//     },
+//   }
+
+//   >>fetch('https://icanhazdadjoke.com', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       jokeEl.innerHTML = data.joke
+//     })
+// }
